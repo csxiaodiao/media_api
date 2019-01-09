a1b2511c6eeb90ecba7c0c097deebdce985b3ae7
csxiaodiao <i@xiaodiao.me>
2019-01-09 14:44:42 +0800

    fix

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,13 +2,5 @@ class MediaController < ApplicationController
   def search
     @q = Medium.ransack(params[:q])
     @medium = Kaminari.paginate_array(@q.result, total_count: @q.result.count).page(params[:page]).per(50)
-    render json:{
-      item: render_to_string(partial: 'tr', locals: {items: @medium.page}),
-      total_count: @medium.total_count,
-      total_pages: @medium.total_pages,
-      current_page: @medium.current_page,
-      prev_page: @medium.prev_page,
-      next_page: @medium.next_page 
-    }
   end
 end
---
+++ app/views/media/search.json.jbuilder
@@ -0,0 +1,6 @@
+json.items render partial: 'tr', locals: {items: @medium.page}
+json.total_count @medium.total_count 
+json.total_pages @medium.total_pages 
+json.current_page @medium.current_page 
+json.prev_page @medium.prev_page 
+json.next_page @medium.next_page 
