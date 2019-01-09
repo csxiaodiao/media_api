b277ccc9418eab2d2ec8f8b20d4b6b9cbee37e61
csxiaodiao <i@xiaodiao.me>
2019-01-09 14:46:45 +0800

    fix

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -2,5 +2,8 @@ class MediaController < ApplicationController
   def search
     @q = Medium.ransack(params[:q])
     @medium = Kaminari.paginate_array(@q.result, total_count: @q.result.count).page(params[:page]).per(50)
+    respond_to do |format|
+      format.json
+    end
   end
 end
