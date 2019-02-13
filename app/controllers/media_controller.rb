f4f2485f28c026ce08416a42230f30541b932418
csxiaodiao <i@xiaodiao.me>
2019-02-13 16:02:23 +0800

    fix

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,6 +1,6 @@
 class MediaController < ApplicationController
   def search
     @q = Medium.ransack(params[:q])
-    @medium = @q.result.page(params[:page]).per(50)    
+    @medium = @q.result.page(params[:page]).per(50)        
   end
 end
